1b2530366ffeb67168e74cb8eab951bf2ea28f0e
joelBit <joel.rollny@gmail.com>
2026-03-30 11:38:01 +0200

    Installed express types and moved server configuration to app.ts

---
+++ app.ts
@@ -0,0 +1,45 @@
+import { createServer } from 'node:https';
+import { readFileSync } from 'node:fs';
+import express from 'express';
+import { Server } from 'socket.io';
+import {checkUsername} from "./src/serverApplication/auth/auth.ts";
+import {initializeHomeEvents} from "./src/serverApplication/events/homeEvents.ts";
+import {initializeDmEvents} from "./src/serverApplication/events/dmEvents.ts";
+import {initializeGamesEvents} from "./src/serverApplication/events/gamesEvents.ts";
+import {initializeCommonEvents} from "./src/serverApplication/events/commonEvents.ts";
+import {initializeMainNamespaceEvents} from "./src/serverApplication/events/mainEvents.ts";
+import {initializeWebRtcEvents} from "./src/serverApplication/events/webRtcEvents.ts";
+
+const app = express();
+app.use(express.static(import.meta.dirname + "/dist/"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true}));
+
+app.all('/{*path}', async (req, res) => {
+    res.redirect(`https://${req.get('host')}`);
+});
+
+const key = readFileSync('cert.key');
+const cert = readFileSync('cert.crt');
+
+const expressServer = createServer({key, cert}, app);
+//create our socket.io server... it will listen to our express port
+const io = new Server(expressServer, {
+    cors: {
+        origin: [
+            "https://localhost",
+            // 'https://LOCAL-DEV-IP-HERE' //if using a phone or another computer
+        ],
+        methods: ["GET", "POST"]
+    }
+});
+
+checkUsername(io);
+initializeMainNamespaceEvents(io);
+initializeCommonEvents(io);
+initializeHomeEvents(io);
+initializeDmEvents(io);
+initializeGamesEvents(io);
+initializeWebRtcEvents(io);
+
+export default expressServer;
--- server.js
+++ server.js
@@ -1,47 +1,7 @@
-import { createServer } from 'node:https';
-import { readFileSync } from 'node:fs';
-import express from 'express';
-import { Server } from 'socket.io';
-import {checkUsername} from "./src/serverApplication/auth/auth.ts";
-import {initializeHomeEvents} from "./src/serverApplication/events/homeEvents.ts";
-import {initializeDmEvents} from "./src/serverApplication/events/dmEvents.ts";
-import {initializeGamesEvents} from "./src/serverApplication/events/gamesEvents.ts";
-import {initializeCommonEvents} from "./src/serverApplication/events/commonEvents.ts";
-import {initializeMainNamespaceEvents} from "./src/serverApplication/events/mainEvents.ts";
-import {initializeWebRtcEvents} from "./src/serverApplication/events/webRtcEvents.ts";
+import expressServer from "./app";
 
-const app = express();
-app.use(express.static(import.meta.dirname + "/dist/"));
-app.use(express.json());
-app.use(express.urlencoded({ extended: true}));
+const PORT = 8181;
 
-app.all('/{*path}', async (req, res) => {
-    res.redirect(`https://${req.get('host')}`);
-});
-
-const key = readFileSync('cert.key');
-const cert = readFileSync('cert.crt');
-
-const expressServer = createServer({key, cert}, app);
-//create our socket.io server... it will listen to our express port
-const io = new Server(expressServer, {
-    cors: {
-        origin: [
-            "https://localhost",
-            // 'https://LOCAL-DEV-IP-HERE' //if using a phone or another computer
-        ],
-        methods: ["GET", "POST"]
-    }
-});
-
-checkUsername(io);
-initializeMainNamespaceEvents(io);
-initializeCommonEvents(io);
-initializeHomeEvents(io);
-initializeDmEvents(io);
-initializeGamesEvents(io);
-initializeWebRtcEvents(io);
-
-expressServer.listen(8181, () => {
-    console.log(`Server listening on port ${8181}`);
+expressServer.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`);
 });
