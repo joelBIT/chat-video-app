5f746dd11124ebc35e2ffd4511da59ebddd0df2d
joelBit <joel.rollny@gmail.com>
2026-03-31 12:48:50 +0200

    Installed mongoose and created connection with mongo database

--- server.js
+++ server.js
@@ -1,4 +1,13 @@
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
 import expressServer from "./app";
+
+dotenv.config({ path: './config.env' });
+
+const DATABASE_URI = process.env.DATABASE_URI;
+
+mongoose.connect(DATABASE_URI)
+  .then(() => console.log('Connected to database'));
 
 const PORT = process.env.PORT ?? 8181;
 
