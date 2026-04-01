e2b682f43abf15e52fd1bc74404feec7c7010463
joelBit <joel.rollny@gmail.com>
2026-04-01 09:58:34 +0200

    Check POST body so that it contains username and password properties

--- app.ts
+++ app.ts
@@ -9,46 +9,14 @@ import {initializeGamesEvents} from "./src/serverApplication/events/gamesEvents.
 import {initializeCommonEvents} from "./src/serverApplication/events/commonEvents.ts";
 import {initializeMainNamespaceEvents} from "./src/serverApplication/events/mainEvents.ts";
 import {initializeWebRtcEvents} from "./src/serverApplication/events/webRtcEvents.ts";
-import type { ChatUser } from './src/types.ts';
-import User from './src/serverApplication/schemas/userSchema.ts';
+import userRouter from "./src/serverApplication/routes/userRoutes.ts";
 
 const app = express();
 app.use(express.static(import.meta.dirname + "/dist/"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
 
-app.post('api/v1/register', async (req, res) => {
-    const userDetails: {username: string, password: string} = Object.assign(req.body);
-    const user: ChatUser | null = await User.findOne({username: userDetails.username});
-    if (user) {
-        res.status(400).json({
-            success: false,
-            message: "Username is not available. Choose a different username.",
-            data: userDetails
-        });
-    } else {
-        const dbUser = new User({
-            username: userDetails.username,
-            password: userDetails.password
-        });
-
-        await dbUser.save().then(doc => {
-            const createdUser: ChatUser = {username: doc.username, id: doc._id.toString(), online: doc.online, avatar: doc.avatar, inCall: doc.inCall};
-            res.status(201).json({
-                success: true,
-                message: "Created",
-                data: createdUser
-            });
-        }).catch(error => {
-            console.log(error);
-            res.status(500).json({
-                success: false,
-                message: "An error occurred during registration.",
-                data: userDetails
-            });
-        });
-    }
-});
+app.use('/register', userRouter);
 
 app.all('/{*path}', async (req, res) => {
     res.redirect(`https://${req.get('host')}`);
@@ -58,7 +26,7 @@ const key = readFileSync('cert.key');
 const cert = readFileSync('cert.crt');
 const expressServer = createServer({key, cert}, app);
 
-const ORIGIN = process.env.NODE_ENV === 'production' ? process.env.URL as string : "https://localhost";
+const ORIGIN: string = process.env.NODE_ENV === 'production' ? process.env.URL as string : "https://localhost";
 
 // Create our socket.io server... it will listen to our express port
 const io = new Server(expressServer, {
---
+++ src/serverApplication/routes/userRoutes.ts
@@ -0,0 +1,59 @@
+import express from 'express';
+import type { ChatUser } from '../../types';
+import User from '../schemas/userSchema';
+
+const router = express.Router();
+
+router
+    .route('/')
+    .post(checkBody, registerUser);
+
+/**
+ * Create new user in database if username is not already taken.
+ */
+async function registerUser(req: express.Request, res: express.Response): Promise<void> {
+    const userDetails: {username: string, password: string} = Object.assign(req.body);
+    const user: ChatUser | null = await User.findOne({username: userDetails.username});
+    if (user) {
+        res.status(400).json({
+            success: false,
+            message: `Username ${userDetails.username} is already in use. Choose a different username.`
+        });
+    } else {
+        const dbUser = new User({
+            username: userDetails.username,
+            password: userDetails.password
+        });
+
+        await dbUser.save().then(doc => {
+            const createdUser: ChatUser = {username: doc.username, id: doc._id.toString(), online: doc.online, avatar: doc.avatar, inCall: doc.inCall};
+            res.status(201).json({
+                success: true,
+                message: "Created",
+                data: createdUser
+            });
+        }).catch(error => {
+            console.log(error);
+            res.status(500).json({
+                success: false,
+                message: "An error occurred during registration."
+            });
+        });
+    }
+}
+
+/**
+ * Check if POST body contains required username and password properties. Only execute the registerUser function if properties exist.
+ */
+function checkBody(req: express.Request, res: express.Response, next: express.NextFunction): express.Response | undefined {
+    if (!req.body.username || !req.body.password) {
+        return res.status(400).json({
+            success: false,
+            message: "Username and password must be supplied."
+        });
+    }
+
+    next();
+}
+
+export default router;
